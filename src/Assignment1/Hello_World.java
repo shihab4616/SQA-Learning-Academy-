36249c6bd53af50ee84b99cbd531b8ff3881b2ad
Shihab Ahmed <shihab4616@gmail.com>
2024-05-22 11:00:55 +0600

    Submit Java 1st Assignment

---
+++ src/Assignment1/Hello_World.java
@@ -0,0 +1,15 @@
+package Assignment1;
+
+public class Hello_World {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello World");
+    }
+
+
+
+
+
+
+}
---
+++ src/Assignment1/Identifiers.java
@@ -0,0 +1,23 @@
+package Assignment1;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        int num=10;
+        int Num=20;
+        int _Num=30;
+        int number100=100;
+        int a2b=45;
+        int $num = 59;
+        int shih$b=90;
+
+        //Output all identifiers
+        System.out.println(num);
+        System.out.println(Num);
+        System.out.println(_Num);
+        System.out.println(number100);
+        System.out.println(a2b);
+        System.out.println($num);
+        System.out.println(shih$b);
+
+    }
+}
